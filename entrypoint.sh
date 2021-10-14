2ae304dc024e8971c159be5f3f544ecf48d8e984
Omar Evans <omar.evans@onepeloton.com>
2021-10-14 15:27:26 -0400

    Add debug output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
+echo "Running checkmarx cxflow with debug"
+ls /
+ls /app
+ls -R /home/runner/work
 java -jar /app/cx-flow.jar --spring.profiles.active="${CX_FLOW_ENABLED_VULNERABILITY_SCANNERS}" --scan --github.api-url="${GITHUB_API_URL}/repos/" --cx-team="${TEAM}" --cx-project="${PROJECT}" --app="${APP}" --f=. ${CXFLOW_PARAMS}
