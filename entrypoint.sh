9bb3cf021836eb51e911ff76343123b0924fda4a
Omar Evans <omar.evans@onepeloton.com>
2021-10-14 15:44:35 -0400

    Fix debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 echo "Running checkmarx cxflow with debug"
 ls /
 ls /app
-ls -R /home/runner/work
+ls -R /github
 java -jar /app/cx-flow.jar --spring.profiles.active="${CX_FLOW_ENABLED_VULNERABILITY_SCANNERS}" --scan --github.api-url="${GITHUB_API_URL}/repos/" --cx-team="${TEAM}" --cx-project="${PROJECT}" --app="${APP}" --f=. ${CXFLOW_PARAMS}
