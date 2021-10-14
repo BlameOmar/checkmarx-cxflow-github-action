e913334e4c120548520e5e159664a9ded66628e2
Omar Evans <omar.evans@onepeloton.com>
2021-10-14 16:12:15 -0400

    Dig even deeper

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "Running checkmarx cxflow with debug"
-ls -l /github
+ls -lR /github
 java -jar /app/cx-flow.jar --spring.profiles.active="${CX_FLOW_ENABLED_VULNERABILITY_SCANNERS}" --scan --github.api-url="${GITHUB_API_URL}/repos/" --cx-team="${TEAM}" --cx-project="${PROJECT}" --app="${APP}" --f=. ${CXFLOW_PARAMS}
